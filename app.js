bf625775dc6972bdfc4096bd17ccd556db8ea252
Rb2PtCl6 <ggmetalgg2@gmail.com>
2023-06-03 10:58:15 +0300

    rename files

--- app.js
+++ app.js
@@ -1,58 +1,39 @@
-const express = require("express");
+const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = 3000;
 
-const images = JSON.parse(fs.readFileSync("technical/info"))
+const images = JSON.parse(fs.readFileSync("technical/info"));
 
-function say(res){
-    res.status(401).send('401: Куда ты полез? Сюда тебе не надо!');
+function sendUnauthorized(res) {
+  res.status(401).send('401: Куда ты полез? Сюда тебе не надо!');
 }
 
-app.get('/images', (req, res) =>{
-    const imageData = req.query.data;
-    if (imageData==undefined) return say(res)
-    var parsed_imageData=imageData.split(":")
-    if (images[parsed_imageData[0]]!=undefined){
-        if (images[parsed_imageData[0]].key==parsed_imageData[1]){
-            res.sendFile(path.join(__dirname,"images",images[parsed_imageData[0]].name))
-        }else{
-            res.status(401).send('401: Неверный ключ доступа!');
-        }
-    }else{
-        res.status(404).send('404: Изображение не найдено!')
-    }
-})
+app.get('/images', (req, res) => {
+  const imageData = req.query.data;
 
-app.get("/", (req, res) =>{
-    say(res)
-})
+  if (!imageData) {
+    return sendUnauthorized(res);
+  }
 
-app.get("/images/:imageName", (req, res) =>{
-    say(res)
-})
-app.get("/technical", (req, res) =>{
-    say(res)
-})
-app.get("/technical/info", (req, res) =>{
-    say(res)
-})
-app.get("/generate", (req, res) =>{
-    say(res)
-})
-app.get("/generate/cnv.js", (req, res) =>{
-    say(res)
-})
-app.get("/generate/info", (req, res) =>{
-    say(res)
-})
+  const [imageIndex, key] = imageData.split(":");
 
-/*app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-  });*/
+  const image = images[imageIndex];
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+  if (image && image.key === key) {
+    const imagePath = path.join(__dirname, "images", image.name);
+    res.sendFile(imagePath);
+  } else {
+    res.status(401).send('401: Неверный ключ доступа!');
+  }
+});
 
-// http://localhost:3000/images?data=0:111
+app.use((req, res, next) => {
+  sendUnauthorized(res);
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
